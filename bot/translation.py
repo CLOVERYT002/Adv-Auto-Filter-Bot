c355b135ace3646757fb1b2861ad07bb47afbd5e
CLOVER Dz <104117790+CLOVERYT002@users.noreply.github.com>
2022-04-21 09:36:51 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,7 +5,8 @@
 class Translation(object):
     
     START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
+<i>ഈ ബോട്ട് [MM] CINEMA CLUB ഗ്രൂപ്പിലേക്ക് ഉള്ളത് എന്ന് ഇനി വീണ്ടും വീണ്ടും പറയണോ??
+അപ്പോ പിന്നെ എന്തിനാ വീണ്ടും വീണ്ടും സ്റ്റാർട്ട് കുത്തി കളിക്കാൻ വരുന്നേ... ആ സൈഡിലോട്ട് എങ്ങാനും മാറി ഇരിക്ക്‌ ഇനി🤭🤭
 
 Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
 
@@ -30,11 +31,11 @@ Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/MM_CINEMA_CLUB">sub</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ 𝘚𝘶𝘱𝘱𝘰𝘳𝘵 Group</b> : <i><a herf="https://t.me/Moviez_group_mg">Join</a></i>
 """
